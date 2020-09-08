4ff836a62bc4e6c396fda9f4b919b7c5271729fc
nada <nada.baklouti@esprit.tn>
2020-09-08 15:44:39 +0100

    Express REST API with MongoDB and Mongoose Part 1

--- node-express/conFusionServer/app.js
+++ node-express/conFusionServer/app.js
@@ -6,12 +6,23 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
-
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
+
+var app = express();
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
